2de176e2e9ba082ebc0d10060d7104a5ae89954c
Giovanni Lodi <giovanni.lodi42@gmail.com>
2018-10-19 20:27:43 +1100

    Create empty framework via Xcode template

---
+++ FoodLogger/FoodLogger.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoodLogger.
+FOUNDATION_EXPORT double FoodLoggerVersionNumber;
+
+//! Project version string for FoodLogger.
+FOUNDATION_EXPORT const unsigned char FoodLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoodLogger/PublicHeader.h>
