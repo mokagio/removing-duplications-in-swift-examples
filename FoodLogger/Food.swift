754e091d42743e66eb7c71c137405c2bdff4cd34
Giovanni Lodi <giovanni.lodi42@gmail.com>
2018-10-19 21:59:49 +1100

    Make Logger expect a Loggable input
    This makes it easier to extend the Logger, as the thing required for a
    something to be logged is for it to conform to Loggable.
    
    It also makes it easier to test the behaviour. Logger only needs one
    test, to ensure it uses its Loggable import properly.
    
    Types conforming to Loggable can be tested in isolation, but it can be
    argued testing a static string is not very valuable. The only exception
    might be if you want to adda layer of safety to make sure changes to the
    static value are intentional. An unintentional test would break the
    tests.

--- FoodLogger/Food.swift
+++ FoodLogger/Food.swift
@@ -1,5 +1,14 @@
-struct Pizza { }
+struct Pizza: Loggable {
 
-struct Gelato { }
+    let logMessage = "Pizza is awesome!"
+}
 
-struct Pasta { }
+struct Gelato: Loggable {
+
+    let logMessage = "I love gelato any time of the year"
+}
+
+struct Pasta: Loggable {
+
+    let logMessage = "There's nothing like home made pasta"
+}
--- FoodLogger/Logger.swift
+++ FoodLogger/Logger.swift
@@ -1,3 +1,7 @@
+protocol Loggable {
+    var logMessage: String { get }
+}
+
 class Logger {
 
     let storage: Storage
@@ -6,16 +10,7 @@ class Logger {
         self.storage = storage
     }
 
-    func log(_ input: Any) {
-        switch input {
-        case is Gelato:
-            storage.persist("I love gelato any time of the year")
-        case is Pasta:
-            storage.persist("There's nothing like home made pasta")
-        case is Pizza:
-            storage.persist("Pizza is awesome!")
-        case _:
-            storage.persist("\(input)")
-        }
+    func log(_ input: Loggable) {
+        storage.persist(input.logMessage)
     }
 }
